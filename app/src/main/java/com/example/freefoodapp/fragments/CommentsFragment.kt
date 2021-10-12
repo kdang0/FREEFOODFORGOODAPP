fc4f687c6b3c0fa09b4edfd0d81358ef9cff21e3
Kyle Mikolajczyk <kjmikolajczyk@wpi.edu>
2021-10-12 19:39:27 -0400

    Doccumentation for the Comments fragment

--- app/src/main/java/com/example/freefoodapp/fragments/CommentsFragment.kt
+++ app/src/main/java/com/example/freefoodapp/fragments/CommentsFragment.kt
@@ -21,33 +21,33 @@ import com.example.freefoodapp.R
 
 
 private const val TAG = "CommentsFragment"
-private const val ARG_USERNAME = "User_Username"
-private const val ARG_POSTID = "Post_ID"
-private const val ARG_POSTNAME = "Post_Name"
+private const val ARG_USERNAME = "User_Username" //Username argument passed
+private const val ARG_POSTID = "Post_ID" //PostID argument passed
+private const val ARG_POSTNAME = "Post_Name" //PostName argument passed
 
 class CommentsFragment: Fragment() {
-    private lateinit var eventName: TextView
-    private lateinit var staticComment: TextView
-    private lateinit var comment: Button
-    private lateinit var userComment: EditText
-    private lateinit var commentsRecyclerView: RecyclerView
-    lateinit var DB: DatabaseReference //Registering
-    lateinit var DBComments: DatabaseReference
-    private var username: String = ""
-    private var originPost: String = ""
+    private lateinit var eventName: TextView //TextView to hold the name of the Post
+    private lateinit var staticComment: TextView //TextView to hold comment
+    private lateinit var comment: Button //Post comment button
+    private lateinit var userComment: EditText //User input for comment
+    private lateinit var commentsRecyclerView: RecyclerView //Recyclerview to hold all post comments
+    lateinit var DB: DatabaseReference //Reference to Firebase Database
+    lateinit var DBComments: DatabaseReference //Reference to Firebase Database child of Comments
+    private var username: String = "" //Username of current user
+    private var originPost: String = "" //The PostID for the comment
     private var adapter: CommentsAdapter? = CommentsAdapter(emptyList())
-    private var comments: MutableList<Comment> = emptyList<Comment>().toMutableList()
-    private var commentContent: String = ""
-    private var postName: String = ""
+    private var comments: MutableList<Comment> = emptyList<Comment>().toMutableList() //List of all comments for this post
+    private var commentContent: String = "" //The comment to be posted
+    private var postName: String = "" //The name of the current post
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DB = FirebaseDatabase.getInstance().reference //registering
-        DBComments = DB.child(DatabaseVars.FIREBASE_COMMENTS)
-        DBComments.orderByKey().addChildEventListener(commentListener)
-        username = arguments?.getSerializable(ARG_USERNAME) as String
-        originPost = arguments?.getSerializable(ARG_POSTID) as String
-        postName = arguments?.getSerializable(ARG_POSTNAME) as String
+        DB = FirebaseDatabase.getInstance().reference //Initalize the DB connection
+        DBComments = DB.child(DatabaseVars.FIREBASE_COMMENTS) //Initalize the DB connection for comments
+        DBComments.orderByKey().addChildEventListener(commentListener) //Start the childevent listener for new comments
+        username = arguments?.getSerializable(ARG_USERNAME) as String //get the username passe d
+        originPost = arguments?.getSerializable(ARG_POSTID) as String //get the post passed
+        postName = arguments?.getSerializable(ARG_POSTNAME) as String //get the post's title passed
     }
 
     override fun onCreateView(
@@ -56,6 +56,9 @@ class CommentsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.comment_list, container, false)
+        /**
+         * Reference all the view's UI elemetns
+         */
         commentsRecyclerView = view.findViewById(R.id.recyclerView) as RecyclerView
         eventName = view.findViewById(R.id.eventName) as TextView
         staticComment = view.findViewById(R.id.staticComment) as TextView
@@ -66,6 +69,7 @@ class CommentsFragment: Fragment() {
         eventName.setText(postName)
         comment.setOnClickListener {
 
+            //Dismiss keyboard and erase text field when it is posted
             val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
 
@@ -82,6 +86,10 @@ class CommentsFragment: Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        /**
+         * As text is entered into the EditText do something with it
+         */
         val contentTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 //space left blank
@@ -103,15 +111,22 @@ class CommentsFragment: Fragment() {
         userComment.addTextChangedListener(contentTextWatcher)
     }
 
+    /**
+     * This will allow the UI to update when it is given a list of comments,
+     * and sets the adapter so we can use the internal update afterwards
+     */
     private fun updateUI(comments : List<Comment>) {
         adapter = CommentsAdapter(comments)
         commentsRecyclerView.adapter = adapter
     }
 
+    /**
+     * This waits for comments to be added, edited, or deleted and does the correct funtion when it happens
+     */
     val commentListener = object : ChildEventListener {
         override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
             Log.d(TAG, "onChildAdded comments:" + dataSnapshot.key!!)
-            addCommentToAList(dataSnapshot)
+            addCommentToAList(dataSnapshot) //Send the new comment to the function to handle
         }
 
         override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
@@ -131,10 +146,13 @@ class CommentsFragment: Fragment() {
         }
     }
 
+    /**
+     * Function to decode comment from Firebase Database
+     */
     private fun addCommentToAList(dataSnapshot: DataSnapshot) {
         val comment = Comment.createComment()
-        val map = dataSnapshot.getValue() as HashMap<String, Any>
-        comment.id = dataSnapshot.key
+        val map = dataSnapshot.getValue() as HashMap<String, Any> //Contains all the values of the comment
+        comment.id = dataSnapshot.key //This is the UUID of the comment
         var dateMap = map.get("date") as HashMap<String, Any>
         var tempDate = Date()
         tempDate.time = (dateMap.get("time") as Long)
@@ -142,7 +160,12 @@ class CommentsFragment: Fragment() {
         comment.postID = map.get("postID") as String?
         comment.content = map.get("content") as String?
         comment.user = map.get("user") as String?
+
+        /**
+         * This checks if the comment is for this post. If it is, we want to add it to the recycleview. Else do nothing
+         */
         if (isOfPost(comment, originPost)) {
+
             comments.add(comment)
             //toDoItemList!!.add(todoItem);
             //Put the new post somewhere
@@ -153,6 +176,9 @@ class CommentsFragment: Fragment() {
         //Do something here to update post view
     }
 
+    /**
+     * This function creats a comment and populates it with the right data
+     */
     fun createComment(content: String, user: String, postID: String) {
         val comment = Comment.createComment()
         comment.date = Date()
@@ -166,10 +192,16 @@ class CommentsFragment: Fragment() {
         Log.d(TAG, "Comment uploaded to cloud")
     }
 
+    /**
+     * Function to check if the Post ID of the comment matches the current Post
+     */
     fun isOfPost(comment: Comment, originPost: String): Boolean {
         return (comment.postID == originPost)
     }
 
+    /**
+     * This handles the individual comment fragments
+     */
     private inner class CommentHolder(view: View) : RecyclerView.ViewHolder(view) {
         private lateinit var comment: Comment
         private val comDateTime : TextView = itemView.findViewById(R.id.comdatetime)
@@ -179,6 +211,9 @@ class CommentsFragment: Fragment() {
         init {
         }
 
+        /**
+         * Here we set each fragment's UI elements to the Comment's data
+         */
         fun bind(comment: Comment) {
             this.comment = comment
             comDateTime.text = comment.date.toString()
@@ -187,6 +222,9 @@ class CommentsFragment: Fragment() {
         }
     }
 
+    /**
+     * This is the adaper that handles the comments
+     */
     private inner class CommentsAdapter(var comments: List<Comment>) :
         RecyclerView.Adapter<CommentHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentHolder {
@@ -204,6 +242,9 @@ class CommentsFragment: Fragment() {
         }
     }
 
+    /**
+     * This is for passing data
+     */
     companion object {
         fun newInstance(userName: String, postID: String, postName: String): CommentsFragment {
             val args = Bundle().apply {
